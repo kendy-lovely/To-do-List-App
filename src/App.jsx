ccbbdde389ae14d7a58f304fee9c944829fabc18
unknown <rafaadzandanendra13@gmail.com>
2025-10-18 16:13:30 +0700

    week 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const task = [
+    {
+      name : "Calculus Class",
+      desc : "Attending the Calculus Class at K.207",
+      date : "20 Oktober 2025",
+      time : "8:00 AM",
+      status : "Active",
+    },
+    {
+      name : "Physics Class",
+      desc : "Attending the Physics Class at K.102",
+      date : "20 Oktober 2025",
+      time : "1:00 PM",
+      status : "Active",
+    },
+    {
+      name : "Computational Thinking Class",
+      desc : "Attending the Computational Thinking Class at GK.208",
+      date : "21 Oktober 2025",
+      time : "10:00 AM",
+      status : "Active",
+    }
+  ];
+  
+  const divArray = []
+  
+  for (let i = 0; i < task.length; i++) {
+    let currentTask = task[i]
+    divArray.push(
+      <div className='bg-slate-700 p-4 rounded-2xl shadow-lg'>
+        <span className='font-semibold text-2xl'>
+          {currentTask.name}
+        </span>
+        <hr/><br/>
+        <span className='text-base text-gray-200'>
+          Description: {currentTask.desc}<br/>
+          Date: {currentTask.date}<br/>
+          Time: {currentTask.time}<br/>
+          Status: {currentTask.status}<br/>
+        </span>
+        <input type='checkbox'/>
+      </div>
+    )
+  }
+
   return (
-    <div className='flex justify-center w-full min-h-screen bg-gray-800 text-white'>
+    <div className='flex justify-center w-full min-h-screen bg-gray-800 text-white pb-[50vh]'>
       <header className='absolute top-0 text-xl p-5 bg-gray-600 w-full text-center rounded-large'>
       <p>To-Do List App</p>
       </header>
@@ -23,20 +68,23 @@ function App() {
           <div className='w-[85%] flex flex-col gap-y-4'>
             <p className='font-semibold text-xl'>Your To-Do List</p>
             <hr/>
-            <div className='bg-slate-700 p-4 rounded-2xl shadow-lg'>
-            <span classNmae='font-semibold text-2xl'>
-              Judul
-            </span>
-            <br/>
-            <br/>
-            <span className='text-base'>
-              Why did the chicken cross the road?<br/>
-              Date: <br/>
-              Time: <br/>
-              Status: To get to the other side! <br/>
-            </span>
-            <input type='checkbox'/>
+
+            {task.map((currentTask, index) =>
+            <div className='bg-slate-700 p-4 rounded-2xl shadow-lg' key={index}>
+              <span className='font-semibold text-2xl mb-2 inline-block'>
+                {currentTask.name}
+              </span>
+              <hr/>
+              <span className='text-base text-gray-200 mt-4 inline-block'>
+                Description: {currentTask.desc}<br/>
+                Date: {currentTask.date}<br/>
+                Time: {currentTask.time}<br/>
+                Status: {currentTask.status}<br/>
+                <input type='checkbox'/><br/>
+              </span>
             </div>
+            )}
+
           </div>
         </div>
       </main>
